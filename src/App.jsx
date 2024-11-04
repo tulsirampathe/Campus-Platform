bc7aeccbf64ffefc1c0b5bba0dabb27ae8215126
tulsirampathe <tulsirampathe81@gmail.com>
2024-11-04 19:54:41 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import ContestOverview from "./components/CreateContest/ContestOverview";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminContestPanel from "./components/Admin/AdminContestPanel";
-import MainCode from "./components/VsCode/MainCode";
-import AdminProfile from "./components/Admin/AdminProfile";
 import AdminContestSetup from "./components/Admin/AdminContestSetup";
+import AdminProfile from "./components/Admin/AdminProfile";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import ContestOverview from "./components/CreateContest/ContestOverview";
+import MainCode from "./components/VsCode/MainCode";
+import AdminLogin from "./components/Admin/AdminLogin";
+
+let user = true;
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AdminProfile />} />
-        <Route path="/admin-contest-setup" element={<AdminContestSetup />} />
-        <Route path="/admin-contest-panel" element={<AdminContestPanel />} />
-        <Route path="/contest-overview" element={<ContestOverview />} />
-        <Route path="/solve" element={<MainCode />} />
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<AdminProfile />} />
+          <Route path="/admin-contest-setup" element={<AdminContestSetup />} />
+          <Route path="/admin-contest-panel" element={<AdminContestPanel />} />
+          <Route path="/contest-overview" element={<ContestOverview />} />
+          <Route path="/solve" element={<MainCode />} />
+        </Route>
+
+        <Route
+          path="/login"
+          element={
+            <ProtectRoute user={!user} redirect="/">
+              <AdminLogin />
+            </ProtectRoute>
+          }
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
